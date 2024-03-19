7b7a6d6fe68ebdc1c668862c6b9290e478e67b6b
moshudougee <mbuthiadenis@gmail.com>
2024-03-20 02:52:02 +0300

    2nd commit

--- models/Notification.js
+++
@@ -1,9 +0,0 @@
-import mongoose, {model, models, Schema} from "mongoose";
-
-const NotificationSchema = new Schema({
-    body: {type: String},
-    userId: {type:mongoose.Types.ObjectId, ref: 'User'},
-    user: {type: mongoose.Types.ObjectId, ref: 'User'},
-},{timestamps: true});
-
-export const Notification = models?.Notification || model('Notification', NotificationSchema);
